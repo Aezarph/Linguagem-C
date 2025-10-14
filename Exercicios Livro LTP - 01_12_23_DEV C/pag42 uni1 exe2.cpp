e1220327586db83c1e119ce9d8bd4e89b93202e6
Aezarph <hiraiijtzy@gmail.com>
2025-10-14 19:51:12 -0300

    Remove pasta de exercicios

--- Exercicios Livro LTP - 01_12_23_DEV C/pag42 uni1 exe2.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-char nome[20]; /*Nome com até 20 letras*/
-printf("\n Digite seu nome: ");
-scanf("%s", nome);
-printf("\n Bem-vindo a disciplina de Algoritmos e Logica de Programacao II %s", nome);
-return (0);
-}
